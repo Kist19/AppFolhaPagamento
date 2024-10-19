9876d555f4acd279ea8386c14067cc15c1f97c5e
Kist19 <kistcross@gmail.com>
2024-10-19 15:54:28 -0300

    finalizado

--- app/src/main/java/br/ulbra/appfolhapagamento/MainActivity.java
+++ app/src/main/java/br/ulbra/appfolhapagamento/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -32,51 +33,84 @@ public class MainActivity extends AppCompatActivity {
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calcularinss();
-                Calcularir();
+                Calcular();
             }
         });
 
     }
 
-    public void Calcularinss() {
-        double salariobruto, salariofinal = 0;
+    public void Calcular() {
+        double salariobruto, inss = 0, ir = 0, salariofinal, nfilhos = 0;
+        String generofinal = "", nome;
+
         try {
             salariobruto = Double.parseDouble(edtSalarioBruto.getText().toString());
+            nome = edtNome.getText().toString();
+
+            if (nome.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Por favor, insira seu nome.", Toast.LENGTH_SHORT).show();
+                return; // Encerrar o método se o nome não for fornecido
+            }
+
+            try {
+                String nFilhosText = edtNFilhos.getText().toString();
+                if (!nFilhosText.isEmpty()) {
+                    nfilhos = Double.parseDouble(nFilhosText);
+                }
+            } catch (NumberFormatException e) {
+                nfilhos = 0;
+            }
+
+            int genero = rgSexo.getCheckedRadioButtonId();
+            if (genero == R.id.rbMasculino) {
+                generofinal = "Sr.";
+            } else if (genero == R.id.rbFeminino) {
+                generofinal = "Sra.";
+            } else {
+                Toast.makeText(MainActivity.this, "Por favor, insira um genero.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
             if (salariobruto <= 1212.00) {
-                salariofinal = salariofinal + (salariobruto * 0.075);
+                inss = salariobruto * 0.075;
             } else if (salariobruto >= 1212.01 && salariobruto <= 2427.35) {
-                salariofinal = salariofinal + (salariobruto * 0.09);
+                inss = salariobruto * 0.09;
             } else if (salariobruto >= 2427.36 && salariobruto <= 3641.03) {
-                salariofinal = salariofinal + (salariobruto * 0.12);
-            } else {
-                salariofinal = salariofinal + (salariobruto * 0.14);
+                inss = salariobruto * 0.12;
+            } else if (salariobruto >= 3641.04 && salariobruto <= 7087.22) {
+                inss = salariobruto * 0.14;
             }
-            if (salariobruto >= 1903.99 && salariobruto <= 2826.65) {
-                salariofinal = salariofinal + (salariobruto * 0.075);
-            } else if (salariobruto >= 2826.66 && salariobruto <= 3751.05) {
-                salariofinal = salariofinal + (salariobruto * 0.15);
-            } else {
-                salariofinal = salariofinal + (salariobruto * 0.225);
+
+            double salarioCominss = salariobruto - inss;
+
+            if (salarioCominss <= 1903.98) {
+                ir = 0;
+            } else if (salarioCominss >= 1903.99 && salarioCominss <= 2826.65) {
+                ir = (salarioCominss - 1903.98) * 0.075;
+            } else if (salarioCominss >= 2826.66 && salarioCominss <= 3751.05) {
+                ir = (salarioCominss - 2826.65) * 0.15 + (2826.65 - 1903.98) * 0.075;
+            } else if (salarioCominss >= 3751.06 && salarioCominss <= 4664.68) {
+                ir = (salarioCominss - 3751.05) * 0.225 + (3751.05 - 2826.65) * 0.15 + (2826.65 - 1903.98) * 0.075;
             }
+
+            salariofinal = salarioCominss - ir;
+
+            if (salariobruto <= 1212.00 && nfilhos >= 0) {
+                for (int i = 0; i < nfilhos; i++) {
+                    salariofinal = salariofinal + 56.47;
+                }
+            }
+
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setTitle(generofinal + " " + nome);
+            dialog.setMessage(String.format("Seu novo salário: R$%.2f", salariofinal));
+            dialog.setNeutralButton("OK", null);
+            dialog.show();
+
         } catch (NumberFormatException e) {
             Toast.makeText(MainActivity.this, "Por favor, insira um número válido.", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void Calcularir() {
-        double salariobruto, salariofinal = 0;
-        try {
-            salariobruto = Double.parseDouble(edtSalarioBruto.getText().toString());
-            if (salariobruto >= 1903.99 && salariobruto <= 2826.65) {
-                salariofinal = salariofinal + (salariobruto * 0.075);
-            } else if (salariobruto >= 2826.66 && salariobruto <= 3751.05) {
-                salariofinal = salariofinal + (salariobruto * 0.15);
-            } else {
-                salariofinal = salariofinal + (salariobruto * 0.225);
-            }
-        } catch (NumberFormatException e) {
-            Toast.makeText(MainActivity.this, "Por favor, insira um número válido.", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
